10c08d69358f14ddeb52ff2b304a6221d5f1baca
AmidoriA <ami.o2sx@gmail.com>
2014-08-09 14:45:59 +0700

    Encrypt Password (reverted from commit 193b55ebf094e969adeea3c9a8e2c825eff71c47)

--- classes/registerHelper.php
+++ classes/registerHelper.php
@@ -5,18 +5,7 @@ class RegisterHelper {
 
 	}
 
-/**
- * Encrypt password 
- *
- * @access public
- * @param  string password
- * @return string encrypted password
- */
-	public function encrypt_password($password) {
-		global $secret;
-		$password = $password . $secret;
-		return md5($password);
-	}
+
 
 }
 ?>
--- model/User.php
+++ model/User.php
@@ -3,6 +3,41 @@ require_once('config.inc.php');
 
 class User {
 
+	private $_username = '';
+	private $_password = '';
+	private $_email = '';
 
+/**
+ * construct
+ * 
+ * @access public
+ * @param string $username
+ * @param string $password
+ * @param string $password
+ */
+	public function __construct($username, $password, $email) {
+		$this->_username = $username;
+		$this->_password = $password;
+		$this->_email = $email;
+	}
+
+/**
+ * Encrypt password 
+ *
+ * @access public
+ * @param  string password
+ * @return string encrypted password
+ */
+	public function encrypt_password($password) {
+		global $secret;
+		$password = $password . $secret;
+		return md5($password);
+	}
+
+	public function save() {
+		global $db_config;
+		mysql_connect($db_config['host'], $db_config['username'], $db_config['password']) or die(mysql_error());
+		mysql_select_db($db_config['name']);
+	}
 }
 ?>
