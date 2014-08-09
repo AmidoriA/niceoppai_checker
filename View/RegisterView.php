0295bc06fdc68a29623779a1aa7a4001c5b537b8
AmidoriA <ami.o2sx@gmail.com>
2014-08-09 18:16:42 +0700

    Fix RegisterView

--- View/RegisterView.php
+++ View/RegisterView.php
@@ -1,6 +1,6 @@
 <form action='servicecaller.php' method='POST'>
 	<input type='hidden' name='service' value="RegisterService">
-	<input type='hidden' name='return_view' value="RegisterView">
+	<input type='hidden' name='return_view' value="Register">
 	<input type="text" name="username">
 	<input type="password" name="password">
 	<input type="text" name="email">
